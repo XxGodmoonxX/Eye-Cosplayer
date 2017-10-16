1bde04c1f4825b8e9226f60500e2f3ac883d481d
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2017-10-16 20:50:18 +0900

    それぞれ眼を描く部分追加

--- Arduino/Eye-Cosplayer/Eye-Cosplayer.ino
+++ Arduino/Eye-Cosplayer/Eye-Cosplayer.ino
@@ -15,41 +15,17 @@ int ayanami = 175;
 int syaringan_kakashi = 35;
 int syaringan_sasuke = 135;
 
-int grip = 10;
-int delay_num = 1000;
+int grip = 10; //gripの角度
+int delay_num = 1000; //それぞれの間のディレイ
 
 int s; //shoulderの角度
 int e; //elbowの角度
 
 void setup() {
-  //Base (M1):90 degrees 0~180
-  //Shoulder (M2): 45 degrees 15~165
-  //Elbow (M3): 180 degrees 0~180
-  //Wrist vertical (M4): 180 degrees 0~180
-  //Wrist rotation (M5): 90 degrees 0~180
-  //gripper (M6): 10 degrees 10~73
   Serial.begin(9600);
   Braccio.begin();
-  
-//  Braccio.ServoMovement(1000, 0, 110, 5, 2, wrist_rotation, grip);
-//  Braccio.ServoMovement(1000, 30, 110, 5, 2, wrist_rotation, grip);
 }
 void loop() {
-  /*
-  Step Delay: a milliseconds delay between the movement of each servo.  
-  Allowed values from 10 to 30 msec.
-  M1=base 0 to 180 degrees
-  M2=shoulder from 15 to 165 degrees
-  M3=elbow from 0 to 180 degrees
-  M4=wrist vertical from 0 to 180 degrees
-  M5=wrist rotation from 0 to 180 degrees
-  M6=gripper Allowed values from 10 to 73 degrees. 
-     10: the toungue is open, 73: the gripper is closed.
-  */
-  //  //Wait 1 second
-  //  delay(1000);
-  //(step delay  M1 , M2 , M3 , M4 , M5 , M6);
-  
   //もしProcessingから何か文字を受け取ったら
   if (Serial.available() > 0) {
     //データの受信を開始
@@ -96,6 +72,73 @@ void loop() {
       Braccio.ServoMovement(3000, 13, s, 90, 133, 90, grip);
       delay(delay_num);
       
+      //ここから眼を描く
+      
+      //上の辺
+      
+      s = 30;
+      e = 20;
+      Braccio.ServoMovement(3000, 18,s,e,180-s-5, pen, grip);
+      delay(delay_num);
+      s = 42;
+      e = 15;
+      Braccio.ServoMovement(3000, 10,s,e,180-s, pen, grip);
+      delay(delay_num);
+      s = 46;
+      e = 11;
+      Braccio.ServoMovement(3000, 8,s,e,180-s, pen, grip);
+      delay(delay_num);
+      s = 49;
+      e = 7;
+      Braccio.ServoMovement(3000, 12,s,e,180-s, pen, grip);
+      delay(delay_num);
+      s = 55;
+      e = 4;
+      Braccio.ServoMovement(3000, 15,s,e,180-s, pen, grip);
+      delay(delay_num);
+      //反対側手前の点
+      s = 58;
+      e = 0;
+      Braccio.ServoMovement(3000, 18,s,e,180-s, pen, grip);
+      delay(delay_num);
+      s = 53;
+      e = 0;
+      Braccio.ServoMovement(3000, 18,s,e,180-s, pen, grip);
+      delay(delay_num);
+      
+      //下の辺
+      
+      s = 50;
+      e = 4;
+      Braccio.ServoMovement(3000, 20,s,e,180-s+10, pen, grip);
+      delay(delay_num);
+      s = 46;
+      e = 7;
+      Braccio.ServoMovement(3000, 22,s,e,180-s+10, pen, grip);
+      delay(delay_num);
+      s = 42;
+      e = 11;
+      Braccio.ServoMovement(3000, 24,s,e,180-s+5, pen, grip);
+      delay(delay_num);
+      s = 35;
+      e = 20;
+      Braccio.ServoMovement(3000, 15,s,e,180-s-5, pen, grip);
+      delay(delay_num);
+      s = 30;
+      e = 25;
+      Braccio.ServoMovement(3000, 12,s,e,180-s-10, pen, grip);
+      delay(delay_num);
+      
+      //ここまで
+      
+      s = 90;
+      Braccio.ServoMovement(3000, 12,s,e,180-s-10, pen, grip);
+      delay(delay_num);
+      
+      //最後腕を振り上げる
+      Braccio.ServoMovement(3000, 0, 90, 30, 90, pen, grip);
+      delay(delay_num);
+      
     }
     if (val == 3) {
       //写輪眼サスケ
@@ -134,6 +177,73 @@ void loop() {
       
       //最後腕を振り上げる
       Braccio.ServoMovement(3000, 13, s, 90, 133, 90, grip);
+      delay(delay_num);
+      
+      //ここから眼を描く
+      
+      //上の辺
+      
+      s = 30;
+      e = 20;
+      Braccio.ServoMovement(3000, 18,s,e,180-s-5, pen, grip);
+      delay(delay_num);
+      s = 42;
+      e = 15;
+      Braccio.ServoMovement(3000, 10,s,e,180-s, pen, grip);
+      delay(delay_num);
+      s = 46;
+      e = 11;
+      Braccio.ServoMovement(3000, 8,s,e,180-s, pen, grip);
+      delay(delay_num);
+      s = 49;
+      e = 7;
+      Braccio.ServoMovement(3000, 12,s,e,180-s, pen, grip);
+      delay(delay_num);
+      s = 55;
+      e = 4;
+      Braccio.ServoMovement(3000, 15,s,e,180-s, pen, grip);
+      delay(delay_num);
+      //反対側手前の点
+      s = 58;
+      e = 0;
+      Braccio.ServoMovement(3000, 18,s,e,180-s, pen, grip);
+      delay(delay_num);
+      s = 53;
+      e = 0;
+      Braccio.ServoMovement(3000, 18,s,e,180-s, pen, grip);
+      delay(delay_num);
+      
+      //下の辺
+      
+      s = 50;
+      e = 4;
+      Braccio.ServoMovement(3000, 20,s,e,180-s+10, pen, grip);
+      delay(delay_num);
+      s = 46;
+      e = 7;
+      Braccio.ServoMovement(3000, 22,s,e,180-s+10, pen, grip);
+      delay(delay_num);
+      s = 42;
+      e = 11;
+      Braccio.ServoMovement(3000, 24,s,e,180-s+5, pen, grip);
+      delay(delay_num);
+      s = 35;
+      e = 20;
+      Braccio.ServoMovement(3000, 15,s,e,180-s-5, pen, grip);
+      delay(delay_num);
+      s = 30;
+      e = 25;
+      Braccio.ServoMovement(3000, 12,s,e,180-s-10, pen, grip);
+      delay(delay_num);
+      
+      //ここまで
+      
+      s = 90;
+      Braccio.ServoMovement(3000, 12,s,e,180-s-10, pen, grip);
+      delay(delay_num);
+      
+      //最後腕を振り上げる
+      Braccio.ServoMovement(3000, 0, 90, 30, 90, pen, grip);
       delay(delay_num);
       
     }
@@ -210,6 +320,30 @@ void loop() {
       
       //最後腕を振り上げる
       Braccio.ServoMovement(3000, 13, s, 90, 133, 90, grip);
+      delay(delay_num);
+      
+      //顔の皺部分
+      
+      s = 30;
+      e = 20;
+      Braccio.ServoMovement(3000, 23,s,e,180-s+10, wrist_rotation, grip);
+      delay(delay_num);
+      //
+      s = 35;
+      e = 16;
+      Braccio.ServoMovement(3000, 25,s,e,180-s+5, wrist_rotation, grip);
+      delay(delay_num);
+      //
+      s = 40;
+      e = 14;
+      Braccio.ServoMovement(3000, 27,s,e,180-s-5, wrist_rotation, grip);
+      delay(delay_num);
+      
+      //ここまで
+      
+      //最後腕を振り上げる
+      
+      Braccio.ServoMovement(3000, 0, 90, 30, 90, wrist_rotation, grip);
       delay(delay_num);
       
     }
